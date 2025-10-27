6669db722e6016a947d3bf26bcb0ab12fca50762
aleksxar <72646375+aleksxar@users.noreply.github.com>
2025-10-28 01:44:17 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/calc/',
+  base: '/3dprintcost/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
